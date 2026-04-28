26395d32c0f23114fd8125a5bbe36112e0c0eaec
Kaleab Hailemeskel <kaleabhailemeskel47@gmail.com>
2026-04-28 12:08:16 +0300

    clerk imp related changes

--- subscription-service/bootstrap/app.php
+++ subscription-service/bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\JwtAuthenticate;
+use App\Http\Middleware\ClerkAuthMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'auth.jwt' => JwtAuthenticate::class,
+            'clerk.auth' => ClerkAuthMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
--- subscription-service/config/services.php
+++ subscription-service/config/services.php
@@ -34,5 +34,9 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
+    'clerk' => [
+        'secret_key' => env('CLERK_SECRET_KEY'),
+        'frontend_url' => env('FRONTEND_URL'),
+    ]
 
 ];
