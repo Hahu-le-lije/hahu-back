946282c2efda46b2db234bbf2592885ad2512414
Izzat-Engida <izzatengida7@gmail.com>
2026-04-29 12:03:10 +0300

    audio thing done

--- gaming_service/dist/routes/wordRoutes.js
+++ gaming_service/dist/routes/wordRoutes.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
+import multer from 'multer';
 import { wordDetails } from "../controllers/wordController.js";
+import { speechController } from "../controllers/speechController.js";
 const router = Router();
+const upload = multer({ storage: multer.memoryStorage() });
 router.post("/", wordDetails);
+router.post('/speech', upload.single('audio'), speechController);
 export default router;
 //# sourceMappingURL=wordRoutes.js.map
--- gaming_service/src/controllers/speechController.ts
+++ gaming_service/src/controllers/speechController.ts
@@ -1,33 +1,46 @@
-import * as sdk from 'microsoft-cognitiveservices-speech-sdk';
-import { resolve } from 'node:dns';
+import { type Request, type Response } from "express";
+import {HfInference} from '@huggingface/inference';
+import {distance} from 'fastest-levenshtein';
+import { error } from "node:console";
 
-export const evaluatePronunciation=async(audioBuffer:Buffer,referenceText:string)=>{
-    const speechConfig=sdk.SpeechConfig.fromSubscription(
-        process.env.AZURE_SPEECH_KEY as string,
-        process.env.AZURE_SPEECH_REGION as string
-    )
-    speechConfig.speechRecognitionLanguage='am-ET';
+const hf=new HfInference(process.env.HUG_FACE);
 
-    const audioConfig=sdk.AudioConfig.fromWavFileInput(audioBuffer);
-    const recognizer=new sdk.SpeechRecognizer(speechConfig,audioConfig);
-
-    const pronunciationConfig=new sdk.PronunciationAssessmentConfig(
-        referenceText,
-        sdk.PronunciationAssessmentGradingSystem.HundredMark,
-        sdk.PronunciationAssessmentGranularity.Phoneme,
-    );
-
-    pronunciationConfig.applyTo(recognizer);
-
-    return new Promise((resolve,reject)=>{
-        recognizer.recognizeOnceAsync(result=>{
-            const assessmentResult=sdk.PronunciationAssessmentResult.fromResult(result);
-            resolve({
-                accuracyScore:assessmentResult.accuracyScore,
-                fluencyScore:assessmentResult.fluencyScore,
-                completenessScore:assessmentResult.completenessScore
-            });
-            recognizer.close();
+export const speechController=async(req:Request,res:Response)=>{
+    try{
+        const {targetWord}=req.body;
+        const audioFile=req.file;
+        if(!audioFile){
+            return res.status(400).json({error:"No audio is uploaded"});
+        }
+        if(!targetWord){
+            return res.status(400).json({error:"Target word is required"});
+        }
+        const audioBlob = new Blob([new Uint8Array(audioFile.buffer)], { type: audioFile.mimetype });
+        const result=await hf.automaticSpeechRecognition({
+            model:"openai/whisper-large-v3",
+            inputs:audioBlob,
+            parameters:{
+                language:"am"
+            }
         })
-    })
+        const heardText=result.text.trim();
+        console.log(heardText);
+        const d=distance(targetWord,heardText);
+        const longest=Math.max(targetWord.length,heardText.length);
+
+        const score=longest===0 ? 0: Math.round(((longest-d)/longest)*100);
+
+        res.status(200).json({
+            success:true,
+            score:Math.max(0,score),
+            heard:heardText,
+            expected:targetWord
+        })
+    }catch(err){
+        console.log("Hf inference error: ",err);
+        res.status(500).json({
+            success:false,
+            error: "Ai service is currently unavailable. please try again later"
+        })
+    }
 }
